16033caef1953960080d02f29f64a1378fea0d6f
Vineetha <vins.im.ruh@gmail.com>
2025-08-15 20:49:34 +1000

    data-type and function

---
+++ PythonWorkbench/syntax/data-type/03_bool.py
@@ -0,0 +1,9 @@
+#Example of Boolean data type
+# Boolean variables store True or False
+isActivated = True
+isDeactivated = False
+
+# Type checking
+print(f"Type of isActivated : {isActivated}")
+print(f"Type of isDeactivated : {isDeactivated}")
+
