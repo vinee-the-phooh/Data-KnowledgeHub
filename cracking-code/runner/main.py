38879e50607fb269694f329ef6a281a3c1ee1ae7
Vineetha <vins.im.ruh@gmail.com>
2025-11-25 22:52:34 +1100

    refactored code

--- cracking-code/runner/main.py
+++ cracking-code/runner/main.py
@@ -1,5 +1,10 @@
 from src.pattern.SquareOfSideN import Pattern_01
 from src.pattern.HollowSquare import Pattern_02
+from src.pattern.RectanglePattern import Pattern_03
+from src.function.CelsiusFahrenheit import function_01
+from src.function.AreaOfRectangle import function_02
+from src.listTupleDictionaries.SumofListElements import test_01
+from src.stringQns.ReverseString import StringQns_01
 
 if __name__ =="__main__":
     #Calling the method in SquareOfSideN 
@@ -8,5 +13,24 @@ if __name__ =="__main__":
 
     sq = Pattern_02(5)
     sq.print_pattern()
+
+    sq = Pattern_03(5,4)
+    sq.print_pattern()
+
+    fn = function_01(23)
+    fn.convert()
+
+    fn =   fn = function_02(4,5)
+    fn.getArea()
+
+    numbers = [1,-9,20, -100]
+    test = test_01(numbers)
+    test.get_sum()
+
+    strQn  = StringQns_01("Data")
+    strQn.getReverseStr()
+
+
+
 
     
